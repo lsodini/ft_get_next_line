1df817b6a60a58854408e679f2f80a928373c48f
lsodini <154422471+lsodini@users.noreply.github.com>
2024-02-16 18:05:20 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsodini <marvin@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 18:00:53 by lsodini           #+#    #+#             */
+/*   Updated: 2024/02/13 18:00:53 by lsodini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t n, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsodini <marvin@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 18:43:12 by lsodini           #+#    #+#             */
+/*   Updated: 2024/02/13 18:43:12 by lsodini          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	*str;
+
+	c = '\n';
+	str = (unsigned char *)s;
+	while (*str != (unsigned char)c)
+	{
+		if (*str == '\0')
+		{
+			return (NULL);
+		}
+		str++
+	}
+	return ((char *)str);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);	
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*p;
+	unsigned char	*pp;
+	size_t				n;
+
+	n = count * size;
+	p = malloc(n);
+	if (p == NULL)
+		return (NULL);
+	pp = p;
+	while (n)
+	{
+		*pp++ = 0;
+		--n; 
+	}
+	return (p);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char	*str;
+
+	i = 0;
+	if( start >= ft_strlen(s))
+	{
+		start = ft_strlen(s);
+		len = 0;
+	}
+	if (len + start > ft_strlen(s))
+	{
+		len = ft_strlen(s) - start;
+	}
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while(i < len && s[start + i])
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	return (str);
+}
