3706a7bce644de1d123cf97dae1dd6bbd4ffdffd
lsodini <154422471+lsodini@users.noreply.github.com>
2024-02-21 18:12:06 +0100

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -17,18 +17,24 @@ char	*ft_freejoin(char *str1, char *str2)
 	char	*temp;
 
 	temp = ft_strjoin(str1, str2);
+	if (!temp)
+		return (NULL);
 	free(str1);
 	return (temp);
 }
 
 char	*ft_sep_str(char **testo)
 {
+	if (!testo || !*testo)
+		return (NULL);
 	char	*buffer;
 	char	*temp;
 	char	*ret;
-	int		i;
-	int		j;
+	int		i
+	int		j
 
+	i = 0;
+	j = 0;
 	i = 0;
 	j = 0;
 	temp = *testo;
@@ -42,26 +48,35 @@ char	*ft_sep_str(char **testo)
 	while (j != -1 && temp[i + j])
 		j++;
 	buffer = ft_substr(*testo, 0, i);
+	if (!buffer)
+		return (NULL);
 	if (j != -1)
 		ret = ft_substr(*testo, i, j);
-	free(temp);
+	if (ret == NULL && j != -1)
+	{
+		free (buffer);
+		return (NULL);
+	}
+	free(*testo);
 	*testo = ret;
 	return (buffer);
 }
 
-char	*ft_get_line(int fd; char *testo)
+char	*ft_get_line(int fd, char *testo)
 {
-	int	bytes_read;
+	if (!testo)
+		return (NULL);
+	int		bytes_read;
 	char	*buffer;
 
 	bytes_read = 1;
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
-	while  (bytes_read > 0)
+	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read == -1 || (bytes_read == 0 && ft_strlen(testo) == 0))
+		if (bytes_read == -1 || (bytes_read == 0 && !*testo))
 		{
 			free(testo);
 			free(buffer);
@@ -70,7 +85,7 @@ char	*ft_get_line(int fd; char *testo)
 		buffer[bytes_read] = '\0';
 		testo = ft_freejoin(testo, buffer);
 		if (ft_strchr(buffer, '\n'))
-			break;
+			break ;
 	}
 	free(buffer);
 	return (testo);
@@ -78,35 +93,43 @@ char	*ft_get_line(int fd; char *testo)
 
 char	*get_next_line(int fd)
 {
-		static	char	*testo;
-		char	*buffer;
+	static char		*testo = NULL;
+	char			*buffer;
 
-		buffer = NULL;
-		if (fd <0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-		{
-			free(testo);
-			return (NULL);
-		}
-		if (!testo)
-			testo = ft_calloc(1, 1);
-		if (!testo)
-			return (NULL);
-		testo = ft_get_line(fd, testo);
-		if (!testo)
-			return (NULL);
-		buffer = ft_sep_str(&testo);
-		return (buffer);
+	buffer = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read (fd, 0, 0) < 0)
+	{
+		free(testo);
+		return (NULL);
+	}
+	if (!testo)
+		testo = ft_calloc(1, 1);
+	if (!testo)
+		return (NULL);
+	testo = ft_get_line(fd, testo);
+	if (!testo)
+		return (NULL);
+	buffer = ft_sep_str(&testo);
+	if (!buffer)
+		return (NULL);
+	return (buffer);
 }
-// int main()
-// {
-//     int fd = open("prova1.txt", O_RDONLY);
 
-//     char *str;
+int main() {
+    int fd = ("prova1.txt", O_RDONLY); 
+	//con fd = 0 legge da stdin
+    if (fd < 0) {
+        perror("Errore nell'apertura del file");
+        return 1;
+    }
 
-// 	for (int i = 0; i < 10; i++)
-// 	{
-// 		str = get_next_line(fd);
-//     	printf("Linea letta : (riga %i) :%s",i,  str);
-// 		free(str);
-// 	}
-// }
+    char *str;
+    int i = 0;
+    while ((str = get_next_line(fd)) != NULL) {
+        printf("Linea letta: (riga %i): %s\n", i++, str);
+        free(str);
+    }
+
+    close(fd);
+    return 0;
+}
